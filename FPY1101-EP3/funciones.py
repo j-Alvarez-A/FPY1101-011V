2e55188a08ecb821093bad1a46f6bce23ba566c9
j-Alvarez-A <jorg.alvareza@duocuc.cl>
2025-10-07 21:10:14 -0300

    Delete FPY1101-EP3 directory

--- FPY1101-EP3/funciones.py
+++
@@ -1,2 +0,0 @@
-def registrar_libro():
-    
--- FPY1101-EP3/menu.py
+++
@@ -1,6 +0,0 @@
-#mrnu biblioteca
-
-print("1.Registar libro\n2.Prestar libros\n3.Listar todos los libros\n4.imprimir reportes de prestamos\n5.salir ")
-
-
-
